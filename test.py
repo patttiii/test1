452d0f36ce28c9c0462819c3b8a7a88641abf42b
patttiii <42357486+patttiii@users.noreply.github.com>
2019-03-05 15:44:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import urllib
-
-print('start')
-
 html_quelltext = urllib.urlopen('http://192.168.1.7/addons/xmlapi/statechange.cgi?ise_id=3152&new_value=true').read()
 
-print('done')
+result_sentence = 	"Licht eingeschaltet" 
+
+current_session_id = intentMessage.session_id
+hermes.publish_end_session(current_session_id, result_sentence)
